0c005399fc36cb8d07656233e1cd9758684c34f2
saragupta <saragupta0302@gmail.com>
2023-05-22 03:31:55 +0530

    Add files

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "eeb41641613fac0eb327fb21976c021a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
